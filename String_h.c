e2cea2eea6fb8d1e5923c6140b890465502c422c
jorge daniel gonzalez <jorge daniel gonzalez>
2016-04-17 13:14:47 -0300

    JG: prueba de mas funciones

--- String_h.c
+++ String_h.c
@@ -12,6 +12,7 @@ int str_cmp(const char*, const char*);
 char *str_cpy(char *,const char*);
 size_t str_cspn(const char*, const char*);
 char *str_error(int);
+size_t str_len(const char*);
 char *str_ncat(char*, const char*, size_t);
 int str_ncmp(const char*, const char*, size_t);
 char *str_ncpy(char*, const char*, size_t);
@@ -114,6 +115,91 @@ char *str_pbrk(const char*, const char*);
 //       printf("\n\n\t cadena 1 igual a cadena 2");
 //}
 
+//void main()//str_cspn
+//{
+//
+//    char cad[31],cad2[31];
+//    int mem;
+//    puts("\t\tIngrese cadena 1:\n\n\t\t");
+//    gets(cad);
+//    puts("\t\tIngrese cadena 2:\n\n\t\t");
+//    gets(cad2);
+//    mem = str_cspn(cad,cad2);
+//    printf("\n %d",mem);
+//}
+
+//void main()//str_error
+//{
+//    int mem = 40;
+//    puts(str_error(mem));
+//}
+
+//void main()//str_len
+//{
+//   char cad[31];
+//    puts("\t\tIngrese cadena 1:\n\n\t\t");
+//    gets(cad);
+//    printf("\n\t\t Cantidad de caracteres de la cadena: %d",str_len(cad));
+//}
+
+//void main()//str_ncat
+//{
+//   char cad[31],cad2[31];
+//
+//    puts("\t\tIngrese cadena 1:\n\n\t\t");
+//    gets(cad);
+//
+//    puts("\t\tIngrese cadena 2:\n\n\t\t");
+//    gets(cad2);
+//
+//    puts(str_ncat(cad,cad2,5));
+//}
+
+//void main()//str_ncmp
+//{
+//    char cad[31],cad2[31];
+//    int mem;
+//    puts("\t\tIngrese cadena 1:\n\n\t\t");
+//    gets(cad);
+//    puts("\t\tIngrese cadena 2:\n\n\t\t");
+//    gets(cad2);
+//    mem = str_ncmp(cad,cad2,6);
+//    printf("\n %d",mem);
+//    if(mem > 0 || mem < 0)
+//    {
+//        if(mem > 0)
+//          printf("\n\n\t cadena 1 mayor a cadena 2");
+//        else
+//          printf("\n\n\t cadena 1 menor a cadena 2");
+//    }
+//    else
+//       printf("\n\n\t cadena 1 igual a cadena 2");
+//}
+
+//void main()//str_ncpy
+//{
+//    char cad[31],cad2[31];
+//
+//    puts("\t\tIngrese cadena 1:\n\n\t\t");
+//    gets(cad);
+//    puts("\t\tIngrese cadena 2:\n\n\t\t");
+//    gets(cad2);
+//
+//    puts(str_ncpy(cad2,cad2,5));
+//}
+
+void main()//str_pbrk
+{
+    char cad[31],cad2[31];
+
+    puts("\t\tIngrese cadena 1:\n\n\t\t");
+    gets(cad);
+    puts("\t\tIngrese cadena 2:\n\n\t\t");
+    gets(cad2);
+
+    puts(str_pbrk(cad,cad2));
+}
+
 
 //void *mem_chr(const void *s, int c, size_t n)
 //{
@@ -202,18 +288,17 @@ char *str_pbrk(const char*, const char*);
 ////entonces el comportamiento no está definido.
 //}
 
-//char *str_chr(char *s, int c)
-//{
-//    while(*s && *s != (unsigned char)c)
-//      s++;
-//
-////    return (*s == (unsigned char)c ? s : NULL);
-////    Localiza la primera aparición de c (convertido a unsigned char) en la cadena apuntada
-////por s (incluyendo el carácter nulo).
-////La función retorna un puntero a partir del carácter encontrado. Si no se ha encontrado
-////el carácter, c, entonces retorna un puntero null.
-//}
+char *str_chr(char *s, int c)
+{
+    while(*s && *s != (unsigned char)c)
+      s++;
 
+    return (*s == (unsigned char)c ? s : NULL);
+//    Localiza la primera aparición de c (convertido a unsigned char) en la cadena apuntada
+//por s (incluyendo el carácter nulo).
+//La función retorna un puntero a partir del carácter encontrado. Si no se ha encontrado
+//el carácter, c, entonces retorna un puntero null.
+}
 
 //int str_cmp(const char *s1, const char *s2)
 //{
@@ -247,68 +332,113 @@ char *str_pbrk(const char*, const char*);
 //{
 //    size_t cant = 0;
 //
-//    while(*s1 && !strchr(s2,*s1)) //strchr devuelve el puntero al carcter encontrado o null si no esta.
+//    while(*s1 && !str_chr(s2,*s1)) //strchr devuelve el puntero al caracter encontrado o null si no esta.
 //    {
 //        cant ++;
 //        s1 ++;
 //    }
 //    return cant;
+//
+////    Cuenta el número de caracteres de una subcadena inicial apuntada por s1 que no
+////contenga ninguno de los caracteres en la cadena apuntada por s2.
+////La función retorna el número de caracteres leídos de la subcadena hasta que halla
+////alguno de los caracteres de s2. El carácter nulo no se cuenta.
 //}
 //char *str_error(int numerror)
 //{
-//    char vec[41];
+//    static char vec[41]; // variable estatica no me da warning, no la mata al terminar la funcion?
 //
-//    sprintf(vec,"%.*s\n",sizeof(vec) - 2, numerror >= 0 && numerror < system_error ? sys_errlist[numerror] : "Unknow Error");
+//    sprintf(vec,"%.*s\n",sizeof(vec) - 2, numerror >= 0 && numerror < sys_nerr ? sys_errlist[numerror] : "Unknow Error");
 //
 //    return vec;
+//
+////    Convierte el número de error en errnum a un mensaje de error (una cadena de caracteres).
+////    La función retorna la cadena de caracteres conteniendo el mensaje asociado con el número
+////de error. Esta conversión y el contenido del mensaje dependen de la implementación. La
+////cadena no será modificada por el programa, pero sí puede ser sobrescrito con otra llamada a
+////la misma función.
 //}
 //size_t str_len(const char *s)
 //{
-//    size_t cant;
+//    size_t cant = 0;
+//    char *aux = s;
 //
-//    while(*s ++) //preguntar por procedencia de operadores, esta mal esto
-//      cant ++;
+//    while(*aux)
+//    {
+//       if(*aux != ' ' && *aux != '\0')
+//          cant ++;
 //
+//       aux ++;
+//    }
 //    return cant;
+//
+////    Calcula el número de caracteres de la cadena apuntada por s.
+////    La función retorna el número de caracteres hasta el carácter nulo, que no se incluye.
 //}
 //char *str_ncat(char*s1, const char *s2, size_t n)
 //{
-//    char *aux = s1;
+//    char *aux = s1, *aux2 = s2;
 //
 //    while(*aux)
 //        aux ++;//preguntar por procedencia de operadores
+//
 //    while(*s2 && n --)
-//        *s1 ++ = *((char *)s2) ++;
-//    *s1 = '\0';
-//    return aux;
+//    {
+//       *aux = *aux2;
+//       aux++;
+//       aux2 ++;
+//    }
+//
+//   *aux = '\0';
+//    return s1;
+//
+////    Añade no más de n caracteres (un carácter nulo y los demás caracteres siguientes no
+////son añadidos) de la cadena apuntada por s2 al final de la cadena apuntada por s1. El
+////carácter inicial de s2 sobrescribe el carácter nulo al final de s1. El carácter nulo
+////siempre es añadido al resultado.
+////La función retorna el valor de s1. Si la copia hace que los objetos se superpongan,
+////entonces el comportamiento no está definido.
 //}
-//int str_ncmp(const char *s1, const char *s2, size_t n) // preguntar por como lo hizo el profe
+
+//int str_ncmp(const char *s1, const char *s2, size_t n) // preguntar como trabaja!!!!
 //{
-//    while(*s1 && *s1 ++ == *s2 ++ && n --)
+//   while(*s1 && *s1 ++ == *s2 ++ && n --)
 //        ;
 //    return (n == 0 || *s1 == '\0' ? 0 : (*(unsigned char*)s1) - (*(unsigned char*)s2));
+//
+////    Compara no más de n caracteres (caracteres posteriores al carácter nulo no se tienen en
+////cuenta) de la cadena apuntada por s1 con la cadena apuntada por s2.
+////La función retorna un número entero mayor, igual, o menor que cero, apropiadamente
+////según la cadena apuntada por s1 es mayor, igual, o menor que la cadena apuntada por
+////s2.
 //}
 //
 //char *str_ncpy(char *s1, const char *s2, size_t n)
 //{
 //    char *aux = s1;
 //
-//    while(*s2 ++ && n --)
+//    while(*s2 && n --) //xq puedo modificar aca s2???
 //
-//        *s1 ++ = *(char*)s2;
-//    while(n --)
+//        *s1 ++ = *(char*)s2 ++;
 //
-//     s1* ++ = '\0';
+//     *s1 ++ = '\0';
 //
 //    return aux;
+//
+////    Copia no más de n caracteres (caracteres posteriores al carácter nulo no son copiados)
+////de la cadena apuntada por s2 a la cadena apuntada por s1.
+////La función retorna el valor de s1. Si al copiar una cadena a la otra se superponen,
+////entonces el comportamiento no está definido. Si el array/arreglo apuntado por s2 es una
+////cadena que es más corta que n caracteres, entonces caracteres nulos son añadidos a la
+////copia en el array apuntado por s1.
 //}
 //
-//char *str_pbrk(const char *s1, const char *s2)
-//{
-//    char *aux = (char*)s1, aux2 = (char*)s2;
-//
-//    while(*aux && !strchr(aux2,*aux))// aux++ aca
-//        aux ++;
-//
-//    return *aux ? aux : NULL;
-//}
+char *str_pbrk(const char *s1, const char *s2)
+{
+    char *aux = s1, *aux2 = s2;
+
+    while(*aux && !str_chr(aux2,*aux))// aux++ aca
+        aux ++;
+
+    return *aux ? aux : NULL;
+}
